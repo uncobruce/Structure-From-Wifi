f7dbff915cdd7e1df3d3bd32132069aa108a71d3
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-06-18 13:58:53 -0400

    Update sparse_inverse_k.py

--- inverse k visibility/sparse_inverse_k.py
+++ inverse k visibility/sparse_inverse_k.py
@@ -75,38 +75,6 @@ def getKValue(point, kvalCoords, kVals):
         if kvalCoords[i] == point:
             return kVals[i]
         
-# Coordinates of sparse k value points
-kValueCoords=[(40, 56), (42, 58), (50,54), (35,55), \
-              (42, 40), (45, 38), (38, 15)] 
-    # coords are plotted as (y, x)
-    
-# k values corresponding to every coordinate
-kVals = [1,1,1,1, \
-         0, 0, 0]
-    
-# Desired grid map dimensions   
-desired_height = 60
-desired_width = 60
-
-# Image of map to be recreated
-img1 = cv2.imread("mapResult.jpg")
-
-# Plot map and k value points for comparison
-data = imageToGrid(img1, desired_height, desired_width)
-routery, routerx = 47,37
-data[routery][routerx] = 1 # transmitter point
-data = plotKCoordinates(kValueCoords, data)
-# plotGrid(data, desired_height, desired_width)
-
-# Create and plot occupancy grid
-data2 = initializeOccupancyGrid(desired_height, desired_width)
-data2 = plotKCoordinates(kValueCoords, data2)
-data2[routery][routerx] = 1 # transmitter point
-
-# Separate k value coords into different lists
-k1vals = getkValCoordinates(kValueCoords, kVals, 1)
-k0vals = getkValCoordinates(kValueCoords, kVals, 0)
-
 def checkRow(data, row, col, kValue, kValueCoords, kVals):
     # Return True if k-1 points on same row found
     x = col
@@ -132,17 +100,79 @@ def checkColumn(data, row, col, kValue, kValueCoords, kVals):
             if kval == kValue - 1:
                 return True
     return False
-                
-            
- 
+
+        
+# Coordinates of sparse k value points
+kValueCoords=[(40, 56), (42, 58), (50,54), (35,55), \
+             (42, 40), (45, 38), (38, 15)] 
+    # coords are plotted as (y, x)
+    
+# k values corresponding to every coordinate
+kVals = [1,1,1,1, \
+         0, 0, 0]
+    
+# Desired grid map dimensions   
+desired_height = 60
+desired_width = 60
+
+# Image of map to be recreated
+img1 = cv2.imread("mapResult.jpg")
+
+# Plot map and k value points for comparison
+data = imageToGrid(img1, desired_height, desired_width)
+routery, routerx = 47,37
+data[routery][routerx] = 1 # transmitter point
+data = plotKCoordinates(kValueCoords, data)
+plotGrid(data, desired_height, desired_width)
+
+# Create and plot occupancy grid
+data2 = initializeOccupancyGrid(desired_height, desired_width)
+data2 = plotKCoordinates(kValueCoords, data2)
+data2[routery][routerx] = 1 # transmitter point
+
+# Separate k value coords into different lists
+k1vals = getkValCoordinates(kValueCoords, kVals, 1)
+k0vals = getkValCoordinates(kValueCoords, kVals, 0)
+
+
+def getNearestYCoordinate(data, point, row, col, kValue, kValueCoords, kVals, routery):
+    # Compare nearest k-1 value by y-coordinate to router y coord
+    # Return y value of whichever is closer to given point
+    x, y = point[1], point[0]
+    routerDifference = int(abs(y - routery))
+    nearest_kVal_y = 0
+    for i in range(len(data[0])):
+        #print(data[i][col])
+        if i == row:
+            continue
+        if data[i][col] != 0: 
+            kval = getKValue((col, i), kValueCoords, kVals)
+            if kval == kValue - 1:
+                nearest_kVal_y = i
+    kValDifference = int(abs(y - nearest_kVal_y))                
+    if routerDifference >= kValDifference:
+        return routery
+    return nearest_kVal_y
+    
+# If there are more issues, recheck checkRow or checkCol   
+
 for p in k1vals:
     isolatedPoints = [] # list for points that don't see any (k-1) points\
     # on same row or column
     row, col = p[1], p[0]
-    print(p)
-    print(checkRow(data2, row, col, 1, kValueCoords, kVals))
-    print(checkColumn(data2, row, col, 1, kValueCoords, kVals))
+    # print(p)
+    # print(checkRow(data2, row, col, 1, kValueCoords, kVals))
+    # print(checkColumn(data2, row, col, 1, kValueCoords, kVals))
+    # print("\n\n\n")
+    checkrow = checkRow(data2, row, col, 1, kValueCoords, kVals)
+    checkcol = checkColumn(data2, row, col, 1, kValueCoords, kVals)
+    if checkcol is True:
+        nearestY = getNearestYCoordinate(data2,p,row,col,1, kValueCoords, kVals, routery)
+        distance = (nearestY - row)//2
+        wally, wallx = row + distance, col
+        data2[wally][wallx] = 1
+    else:
+        isolatedPoints.append(p)
 
 
-
-# plotGrid(data2, desired_height, desired_width)
+plotGrid(data2, desired_height, desired_width)
