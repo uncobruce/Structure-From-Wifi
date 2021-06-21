c197b77ab2a385f0fa3bad9d572f4ef83da5cd60
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-06-21 11:08:25 -0400

    Update sparse_inverse_k.py

--- inverse k visibility/sparse_inverse_k.py
+++ inverse k visibility/sparse_inverse_k.py
@@ -75,10 +75,12 @@ def getKValue(point, kvalCoords, kVals):
         if kvalCoords[i] == point:
             return kVals[i]
         
-def checkRow(data, row, col, kValue, kValueCoords, kVals):
+def checkRow(data, row, col, kValue, kValueCoords, kVals, routery):
     # Return True if k-1 points on same row found
     x = col
     y = row
+    if y == routery:
+        return True
     for i in range(len(data[0])):
         if i == x:
             continue
@@ -88,10 +90,12 @@ def checkRow(data, row, col, kValue, kValueCoords, kVals):
                 return True
     return False
 
-def checkColumn(data, row, col, kValue, kValueCoords, kVals):
+def checkColumn(data, row, col, kValue, kValueCoords, kVals, routerx):
     # Return True if k-1 points on same col found
     x = col
     y = row
+    if x == routerx:
+        return True
     for i in range(len(data[0])):
         if i == y:
             continue
@@ -104,11 +108,13 @@ def checkColumn(data, row, col, kValue, kValueCoords, kVals):
         
 # Coordinates of sparse k value points
 kValueCoords=[(40, 56), (42, 58), (50,54), (35,55), \
+              (42, 7),(37, 7),(57, 40), (55, 38),\
              (42, 40), (45, 38), (38, 15)] 
     # coords are plotted as (y, x)
     
 # k values corresponding to every coordinate
 kVals = [1,1,1,1, \
+         1,1,1,1,\
          0, 0, 0]
     
 # Desired grid map dimensions   
@@ -135,55 +141,62 @@ k1vals = getkValCoordinates(kValueCoords, kVals, 1)
 k0vals = getkValCoordinates(kValueCoords, kVals, 0)
 
 
-def getNearestYCoordinate(data, point, row, col, kValue, kValueCoords, kVals, routery):
+def getNearestYDistance(data, point, kPrevCoords, routery):
     # Compare nearest k-1 value by y-coordinate to router y coord
     # Return y value of whichever is closer to given point
-    x, y = point[1], point[0]
-    routerDifference = int(abs(y - routery))
-    nearest_kVal_y = 0
-    for i in range(len(data[0])):
-        #print(data[i][col])
-        if i == row:
-            continue
-        if data[i][col] != 0: 
-            kval = getKValue((col, i), kValueCoords, kVals)
-            if kval == kValue - 1:
-                nearest_kVal_y = i
-    kValDifference = int(abs(y - nearest_kVal_y))                
-    if routerDifference >= kValDifference:
-        return routery
-    return nearest_kVal_y
-    
+    y = point[1]
+    routerDiff = routery - y
+    lowest_kDiff = None
+    for k in kPrevCoords:
+        ky = k[1]
+        kDiff = ky - y
+        if lowest_kDiff is None:
+            lowest_kDiff = kDiff
+        if kDiff < lowest_kDiff:
+            lowest_kDiff = kDiff
+        #print(lowest_kDiff, routerDiff)
+    if abs(lowest_kDiff) < abs(routerDiff):
+        return lowest_kDiff
+    return routerDiff
+
+def getNearestXDistance(data, point, kPrevCoords, routerx):
+    x = point[0]
+    routerDiff = routerx - x
+    lowest_kDiff = None
+    for k in kPrevCoords:
+        kx = k[0]
+        kDiff = kx - x
+        if lowest_kDiff is None:
+            lowest_kDiff = kDiff
+        if kDiff < lowest_kDiff:
+            lowest_kDiff = kDiff
+    if abs(kDiff) < abs(routerDiff):
+        return kDiff
+    return routerDiff
+            
 # If there are issues, recheck checkRow or checkCol   
-isolatedPoints = [] # list for points that don't see any (k-1) points\
+a = getNearestXDistance(data2, (55,38), k0vals, routerx)
+
+# Build horizontal parts of k1
 wallCoords = []
 for p in k1vals:
-    # on same row or column
     row, col = p[1], p[0]
-    # print(p)
-    # print(checkRow(data2, row, col, 1, kValueCoords, kVals))
-    # print(checkColumn(data2, row, col, 1, kValueCoords, kVals))
-    # print("\n\n\n")
-    checkrow = checkRow(data2, row, col, 1, kValueCoords, kVals)
-    checkcol = checkColumn(data2, row, col, 1, kValueCoords, kVals)
+    checkcol = checkColumn(data2, row, col, 1, kValueCoords, kVals, routerx)
     if checkcol is True:
-        nearestY = getNearestYCoordinate(data2,p,row,col,1, kValueCoords, kVals, routery)
-        distance = (nearestY - row)//2
-        wally, wallx = row + distance, col
+        nearestYDistance = getNearestYDistance(data2, p, k0vals, routery)
+        wally, wallx = (row + nearestYDistance//2), col
         data2[wally][wallx] = 1
         wallCoords.append((wallx,wally))
-        for point in isolatedPoints:
-            x, y = point[0], point[1]
-            data2[wally][x] = 1
-            wallCoords.append((x,wally))
-        isolatedPoints=[]
-    elif checkcol is False:
-        isolatedPoints.append(p)
-    if p == k1vals[-1]:
-        for point in isolatedPoints:
-            x, y = point[0], point[1]
-            data2[wally][x] = 1
-            wallCoords.append((x,wally))
-        isolatedPoints=[]
+        
+# Build vertical parts of k1
+for p in k1vals:
+    row, col = p[1], p[0]
+    checkrow = checkRow(data2, row, col, 1, kValueCoords, kVals, routery)
+    if checkrow is True:
+        nearestXDistance = getNearestXDistance(data2, p, k0vals, routery)
+        wally, wallx = row, col + nearestXDistance//2
+        data2[wally][wallx] = 1
+        wallCoords.append((wallx,wally))
 
+isolatedPoints = [] # for points that don't see any (k-1) points
 plotGrid(data2, desired_height, desired_width)
