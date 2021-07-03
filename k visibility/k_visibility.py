3cced2d04a19cf3daa00cba9f32ac62ee0f58a37
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-07-03 18:24:39 -0400

    Update k_visibility.py

--- k visibility/k_visibility.py
+++ k visibility/k_visibility.py
@@ -5,23 +5,14 @@ import cv2
 import math
 from bresenham import bresenham
 
-def isVertexCritical(criticalRay, p1, p2):
-    # criticalRay: LineString object giving critical ray
-    # return True if vertex is critical vertex 
-    router = criticalRay.coords[0]
-    vi = criticalRay.coords[1]
-    routerx, routery = router[0], router[1]
-    line = {'x1': routerx,'y1':-routery,'x2':vi[0],'y2':-vi[1]}
-    p1 = {'x':p1[0],'y':-p1[1]}
-    p2 = {'x':p2[0],'y':-p2[1]} 
-    return ((line['y1'] - line['y2'])*(p1['x'] - line['x1'])+(line['x2']- line['x1'])*(p1['y']-line['y1']))*((line['y1']-line['y2'])*(p2['x']-line['x1'])+(line['x2']-line['x1'])*(p2['y']-line['y1']))>0
-
 # Create list of points from given vertices
 verticesArray = ([462., 636.], [434., 416.], [599., 382.],[424., 277.],   
                     [709., 197.], [702., 384.], [850., 496.], [877.,  23.],
                     [454.,  14.], [189.,  85.], [ 85., 257.],
                     [100., 474.], [227., 443.], [217., 264.], [351., 264.], [324., 529.],
                     [211., 621.])
+verticesArray = list(verticesArray)
+verticesArray.reverse()
 points = []
 for v in verticesArray:
     px, py = v[0], -v[1]
@@ -42,6 +33,17 @@ for point in coordinates:
     
 # STEP 1) Find and plot critical vertices
 # -------------------------------------------------------------------------
+def isVertexCritical(criticalRay, p1, p2):
+    # criticalRay: LineString object giving critical ray
+    # return True if vertex is critical vertex 
+    router = criticalRay.coords[0]
+    vi = criticalRay.coords[1]
+    routerx, routery = router[0], router[1]
+    line = {'x1': routerx,'y1':-routery,'x2':vi[0],'y2':-vi[1]}
+    p1 = {'x':p1[0],'y':-p1[1]}
+    p2 = {'x':p2[0],'y':-p2[1]} 
+    return ((line['y1'] - line['y2'])*(p1['x'] - line['x1'])+(line['x2']- line['x1'])*(p1['y']-line['y1']))*((line['y1']-line['y2'])*(p2['x']-line['x1'])+(line['x2']-line['x1'])*(p2['y']-line['y1']))>0
+
 routerPoint = Point((routerx, routery))
 i=0
 criticalvertices=[]
@@ -129,9 +131,48 @@ for v in criticalvertices:
      i+=1
                 
 # +2/-2 rule
+def get_v_j_prev(coordinates, q):
+    i=0
+    for coord in coordinates:
+        if i != len(coordinates)-1:
+            pt1 = np.array(coord)
+            pt2 = np.array(coordinates[i+1])
+            pt3 = np.array(q)
+            d12 = int(np.linalg.norm(pt2-pt1))
+            d13 = int(np.linalg.norm(pt3-pt1))
+            d23 = int(np.linalg.norm(pt2-pt3))
+            if d12-1 == d13 + d23 or d12 == d13 + d23:
+                return tuple(pt2)
+        i+=1
+    
 
 
-
+def onSameSide(criticalRay, p1, p2):
+    # criticalRay: LineString object giving critical ray
+    # return True if v_i_prev and v_j_prev are on same side
+    router = criticalRay.coords[0]
+    vi = criticalRay.coords[1]
+    routerx, routery = router[0], router[1]
+    line = {'x1': routerx,'y1':-routery,'x2':vi[0],'y2':-vi[1]}
+    p1 = {'x':p1[0],'y':-p1[1]}
+    p2 = {'x':p2[0],'y':-p2[1]} 
+    return ((line['y1'] - line['y2'])*(p1['x'] - line['x1'])+(line['x2']- line['x1'])*(p1['y']-line['y1']))*((line['y1']-line['y2'])*(p2['x']-line['x1'])+(line['x2']-line['x1'])*(p2['y']-line['y1']))>0
+i=0
+for v in coordinates:
+    if i!= 0 and v in criticalvertices: 
+        v_i_prev = coordinates[i-1]
+        qpts = intpoints[v] # list of intersection points for critical ray
+        print('crit vertex:', v, )
+        if qpts is not []:
+            for q in qpts:
+                v_j_prev = get_v_j_prev(coordinates, q)
+                v2 = extendRay(routerPoint, v)
+                rc = LineString([routerPoint, v2])
+                if onSameSide(rc, v_j_prev, v_i_prev):
+                    pointIDs[q] = -2
+                else:
+                    pointIDs[q] = 2
+    i+=1
 
 
 
