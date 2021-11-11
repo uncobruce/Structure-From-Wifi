3ac6a4d14ce2db0faab9ec3323e3c8a797cfb8d6
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-11-11 09:10:45 -0500

    added grid mapping directory

---
+++ inverse_k_visibility/sparse_inverse/simulated/sparse_inverse_coneshapes/grid_mapping/grid_map.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class GridMap:
+    def __init__(self, refined_polygons_list, trajectory_object, desired_height=80, desired_width=80):
+        self.gridmap = np.zeros(desired_width*desired_height)
+        self.gridmap = self.gridmap.reshape((desired_width, desired_height))
+        self.gridmap[self.gridmap == 0] = 0.5
+        return self.gridmap
+
