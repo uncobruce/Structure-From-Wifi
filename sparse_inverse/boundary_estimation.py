4e28d9eb61b30f6dbad6215c2cc63ef6faf7401b
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-12-24 10:40:18 -0500

    Update boundary_estimation.py

--- sparse_inverse/boundary_estimation.py
+++ sparse_inverse/boundary_estimation.py
@@ -14,13 +14,11 @@ def slope(x1, y1, x2, y2):
     slope = (y2-y1)/(x2-x1)
     return slope
 
-def multiPolygonIntersections(multipolys, ki):
+def multiPolygonWallSlope(multipolys, ki):
     k_j_polygons = list(multipolys.geoms)
     wall_slope = None
     for kj in k_j_polygons:    
-        # print(kj)
         intersection = kj.intersection(ki)
-        
         if intersection.geom_type == 'LineString':
             x1, y1 = intersection.coords[0][0], intersection.coords[0][1]
             x2, y2 = intersection.coords[1][0], intersection.coords[1][1]
@@ -42,17 +40,39 @@ def multiPolygonIntersections(multipolys, ki):
                     elif m == math.inf or m == 0:
                         wall_slope = m
                     elif wall_slope == None or abs(m) > wall_slope:
-                        wall_slope = m
-                        
-    print(wall_slope)
+                        wall_slope = m    
+    if wall_slope == math.inf or abs(wall_slope) > 35:
+        wall_slope = 'vertical'
+    return wall_slope
     
-    
+def getHorizMidPoint(polygon):
+    pass # get polygon midpoint from router-most coord to closest x coord
+
+def closestXPointtoRouter(midpoints):
+    smallest_distance = 0
+    midpoint = None
+    return midpoint
+def multiPolygonMidPointHandler(multipolys, wall_slope):
+    k_j_polygons = list(multipolys.geoms)
+    if wall_slope == 'vertical':
+        midpoints=[]
+        for kj in k_j_polygons:
+            midpoint = getHorizMidPoint(kj)
+            midpoints.append(midpoint)
+        midpoint = closestXPointtoRouter(midpoints)
+
+def multiPolygonHorizWallEstimator(wallHorizMidPoint, multipolys):
+    k_j_polygons = list(multipolys.geoms)    
+    # plot wall along all y coords within poly at same wallhorizmidpoint
+
 def wallCoordinates(coneshapes_dict):
     ki = coneshapes_dict[0][0]
     kj = coneshapes_dict[1][0]
     difference = kj.difference(ki)
     if difference.geom_type == 'MultiPolygon':
-        multiPolygonIntersections(difference, ki)
+        wall_slope = multiPolygonWallSlope(difference, ki)
+        wallHorizMidPoint = multiPolygonMidPointHandler(difference, wall_slope)
+        wallEstimation = multiPolygonHorizWallEstimator(wallHorizMidPoint, difference)
 
 
 
